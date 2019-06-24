bfdbaaeceee9a259f953959c81160e91163ef781
Katherine Ramirez <oeillet8_5@hotmail.com>
2019-06-24 10:26:46 -0400

    probando subir a github

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,60 @@
 window.cipher = {
-  encode: () => {
+  encode: (textMessage, numClave) => {
     /* Acá va tu código */
-    
-  },
-  decode: () => {
-    /* Acá va tu código */
+    /*largo del mensaje */
+     let Resultado="";
+      for (let i = 0; i < textMessage.length ; i++) { 
+        //console.log(textMessage.length);
+        
+        let formula = textMessage.charCodeAt(i)
+        //mayusculas
+        if (formula >= 65 && formula <= 90) {
+          Resultado +=String.fromCharCode((formula - 65 + parseInt(numClave)) % 26 + 65)
+        }
+        //minusculas
+        if (formula >= 97 && formula <=122){
+          Resultado +=String.fromCharCode((formula - 97 + parseInt(numClave)) % 26 + 97)
+        }
+        //espacios
+        if (formula==32 ){
+          Resultado +=String.fromCharCode((formula-32 + numClave)%1 +32)
+        }
+        //numeros
+        if(formula >= 48 && formula <=57){
+          Resultado +=String.fromCharCode(((formula - 48 +parseInt(numClave)+20)%10)+48)
+        }
+        //textMessage+= Resultado
+       // console.log(textMessage+" "+Resultado)
+      }
+      return Resultado
+    },
+   
+   decode: (textMessage, numClave) => {
+    let Resultado="";
+      
+      for (let i = 0; i < textMessage.length; i++) { 
+        
+      let formula = textMessage.charCodeAt(i);
+
+        if (formula >= 65 && formula <= 90) {
+          Resultado += String.fromCharCode((formula - 90 - parseInt(numClave)) % 26 + 90);
+        }
+        if (formula >= 97 && formula <= 122){
+          Resultado += String.fromCharCode((formula - 122 - parseInt(numClave)) % 26 + 122);
+        }
+        if (formula==32) {
+          Resultado=String.fromCharCode((formula-32 + numClave)%1 +32)
+        }
+        if(formula >= 48 &&formula <=57){
+          Resultado += String.fromcharcode(((formula - 48 +parseInt(numClave) + 20) % 10 )+48) 
+        }
+       // textMessage+= Resultado;
+    }
+    return Resultado;
+
   }
-};
+  
+ 
+  }
+
+  
--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-/*Acá va tu código*/
+const textResultado = document.getElementById('resultado');
+const btnCifrar = document.getElementById('cifrar');
+const btnDecifrar = document.getElementById('decifrar');
+  /*la aplicacion al escuchar el boton cifrar leera el mensaje 
+  en el primer recuadro y su clave respectiva luego dara su resultato en el recuadro 2 */
+  btnCifrar.addEventListener('click',() => {
+    let textMessage = document.getElementById('message').value;
+    let numClave = document.getElementById('offset').value;
+    console.log(textMessage+" CLAVE: "+numClave);
+    textResultado.innerHTML = window.cipher.encode(textMessage, numClave);
+  }) 
+  
+ /*  btnCifrar.addEventListener('click', () => {
+    //alert("HOLA MONKEY!!! TE CIFRO");
+    let textMessage = document.getElementById('message').value;
+    let numClave = document.getElementById('offset').value;
+    console.log("CLAVE CIFRADO "+numClave);
+    textResultado.innerHTML = window.cipher.encode(textMessage, numClave);
+  }) */
+
+
+  btnDecifrar.addEventListener("click", () => {
+    let textMessage = document.getElementById('message').value;
+    let numClave = document.getElementById("offset").value;
+    textResultado.innerHTML = window.cipher.decode(textMessage, numClave);
+    
+  })
+    /*el boton limpiar eliminara todo texto aplicado por el usuario*/
+let botonLimpiar = document.getElementById("limpiar");
+    botonLimpiar.addEventListener("click",()=> {
+    document.getElementById("offset").value = "";  
+    document.getElementById("message").value = "";  
+    document.getElementById("resultado").value = "";   
+}) ;
+
+
+
